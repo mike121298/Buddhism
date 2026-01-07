0fc6bf5ea1a1a81597b58b4a7e49d56e6faa71d7
mike121298 <mikemike.121298@gmail.com>
2026-01-07 17:56:22 +0800

    test2

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "DefaultCompany-Buddhism-v0.6";
+const cacheName = "DefaultCompany-Buddhism-v0.7";
 const contentToCache = [
     "Build/Buddhism.loader.js",
     "Build/Buddhism.framework.js",
